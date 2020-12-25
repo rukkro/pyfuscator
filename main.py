1dfa5b64c06f1a4de6724ca4d853e42a65902dfb
rukkro <rukkro@users.noreply.github.com>
2020-12-25 15:36:39 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 import sys, inspect
 
 MAX_CHR = 25565
-OBFUSCATOR_FUNC = "lambda v: chr(v << 2)"
+OBFUSCATOR_FUNC = "lambda v: '|' + (v*'_') + '|'"
+SPLIT_VAR = "8/8"
 VAR_MAPPER_FUNC = f"""[setattr(sys.modules[__name__], ({OBFUSCATOR_FUNC})(v), chr(v)) for v in range({MAX_CHR}) if (sys:=__import__('sys'))]"""
 
 # Global variable Mapper
 exec(VAR_MAPPER_FUNC)
 
 # The code import
-input_file = __import__('shiba_term')
+input_file = __import__('tst')
 lines: str = inspect.getsource(input_file)
 
 # Encoder
-encoded_file: list[str] = [
+encoded_file =[
     eval(f"({OBFUSCATOR_FUNC})({ord(i)})", globals()) for i in lines
 ]
 print(encoded_file)
 
 with open('tst.py','w',encoding='utf-8') as tstfile:
     pre = "# coding=utf8\n"
-    encoded_with_decoder = f""""".join([getattr(sys.modules[__name__], i) for i in {encoded_file}]"""
-    code = f"""{VAR_MAPPER_FUNC} and exec({encoded_with_decoder}),globals())"""
+    encoded_with_decoder = f""""".join(getattr(sys.modules[__name__], i) for i in {encoded_file})"""
+    code = f"""{VAR_MAPPER_FUNC} and exec({encoded_with_decoder},globals())"""
     tstfile.write(pre)
     tstfile.write(code)
 
