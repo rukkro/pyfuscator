c3820e82ee3831cc6fa2cf38e8069a42f6abb51f
rukkro <rukkro@users.noreply.github.com>
2020-12-25 11:25:30 -0600

    WIP....

---
+++ main.py
@@ -0,0 +1,37 @@
+import sys, inspect
+
+MAX_CHR = 25565
+OBFUSCATOR_FUNC = "lambda v: chr(v << 2)"
+VAR_MAPPER_FUNC = f"""[setattr(sys.modules[__name__], ({OBFUSCATOR_FUNC})(v), chr(v)) for v in range({MAX_CHR}) if (sys:=__import__('sys'))]"""
+
+# Global variable Mapper
+exec(VAR_MAPPER_FUNC)
+
+# The code import
+input_file = __import__('shiba_term')
+lines: str = inspect.getsource(input_file)
+
+# Encoder
+encoded_file: list[str] = [
+    eval(f"({OBFUSCATOR_FUNC})({ord(i)})", globals()) for i in lines
+]
+print(encoded_file)
+
+with open('tst.py','w',encoding='utf-8') as tstfile:
+    pre = "# coding=utf8\n"
+    encoded_with_decoder = f""""".join([getattr(sys.modules[__name__], i) for i in {encoded_file}]"""
+    code = f"""{VAR_MAPPER_FUNC} and exec({encoded_with_decoder}),globals())"""
+    tstfile.write(pre)
+    tstfile.write(code)
+
+
+
+with open('tst.py','r',encoding='utf-8') as importfileed:
+
+    # Decoder
+    bb = "".join([
+        getattr(sys.modules[__name__], i) for i in importfileed.read()
+    ])
+    # Executor
+    exec(bb)
+hello = '''abc'''
